898b12be58019f2ce99c6c42ebdc623fc4e87a4d
Mihir2423 <mihirraj444@gmail.com>
2025-02-14 08:58:12 +0530

    chore: update mobile design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,41 @@
 "use client";
 
-import { Mail } from "lucide-react";
+import { Contact, Github, Mail } from "lucide-react";
 import Image from "next/image";
+import { FaDiscord } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center gap-16 w-full">
-      <div className="relative flex flex-col justify-center items-center gap-10 md:pt-60 w-full h-[calc(100vh-65px)] overflow-y-hidden">
+      <div className="relative flex flex-col md:justify-center items-center gap-10 pt-32 md:pt-60 w-full h-[calc(100vh-65px)] overflow-y-hidden">
         <Image
           src="/vector.png"
           alt="hero"
           width={800}
           height={600}
-          className="hidden md:block bottom-[-100px] absolute w-full h-full"
+          className="bottom-[-100px] absolute w-full h-full"
         />
-        <h1 className="bg-clip-text bg-gradient-stop bg-gradient-to-br from-white via-30% via-white to-white/30 font-medium text-transparent md:text-[72px] text-5xl text-center leading-[60px] md:leading-[82px]">
+        <div
+          className="bottom-[30px] absolute flex items-center gap-3 px-6 py-3 border border-[#252525] rounded-[24px]"
+          style={{
+            background:
+              "linear-gradient(to bottom, #535050 0%, rgba(34, 34, 35, 0.6) 99.75%)",
+          }}
+        >
+          <Github size={18} />
+          <FaDiscord size={18} />
+          <Contact size={18} />
+        </div>
+        <h1 className="bg-clip-text bg-gradient-stop bg-gradient-to-br from-white via-30% via-white to-white/30 mx-2 font-medium text-[40px] text-transparent md:text-[72px] text-center leading-[60px] md:leading-[82px]">
           Your Open source <br /> Gmail alternative
         </h1>
-        <div className="flex justify-center items-center gap-12 mx-2 px-3 md:px-8 py-2 border-neutral-400 border-t border-b border-dashed font-medium text-neutral-400 max-md:text-sm">
+        <div className="flex justify-center items-center gap-[24px] md:gap-12 mx-2 px-3 md:px-8 py-2 border-neutral-400 border-t border-b border-dashed font-medium text-neutral-400 max-md:text-[10px]">
           <span className="relative text-center">
             Easy to self host
-            <span className="top-1/2 right-[-24px] absolute border-neutral-400 border-r border-dashed h-[101px] -translate-y-1/2" />
+            <span className="top-1/2 right-[-12px] md:right-[-24px] absolute border-neutral-400 border-r border-dashed h-[101px] -translate-y-1/2" />
           </span>
           <div className="relative flex flex-col justify-center items-center gap-1">
-            <span className="top-1/2 right-[-24px] absolute border-neutral-400 border-r border-dashed h-[101px] -translate-y-1/2" />
+            <span className="top-1/2 right-[-12px] md:right-[-24px] absolute border-neutral-400 border-r border-dashed h-[101px] -translate-y-1/2" />
             <div className="flex items-center gap-2">
               <div className="flex items-center gap-1">
                 <Mail size={12} className="translate-y-[0.5px]" />
@@ -35,7 +47,7 @@ export default function Home() {
           </div>
           <span>Customizable</span>
         </div>
-        <form className="flex justify-center items-center gap-4">
+        <form className="flex max-md:flex-col justify-center items-center gap-4 max-md:mt-6">
           <div
             className="relative rounded-md w-[252px] h-[33px]"
             style={{
